759405aa7c0b2256bc49e9572b1b7d06c3a36cb6
Yin-Tzu Lin(阿孜孜^.^)@CML503 <known@cmlab.csie.ntu.edu.tw>
2015-01-18 19:42:39 +0800

    remove STRAIGHT because no license

--- vtpInit.m
+++ vtpInit.m
@@ -1,16 +1,15 @@
 function vtpInit
 
 %% Add necessary toolboxes to the search path
-toolboxPath.root=''; %root path for your utility,machineLearning, sap toolboxes (by Roger jang), STRAIGHT toolbox, voicebox and pmtk toolbox
-toolboxPath.remote = {'utility','machineLearning','sap','TandemSTRAIGHTmonolithicPackage002Test','voicebox','pmtk3-1nov12'};
-% toolboxPath.remotePrefix = {'jang\','jang\','jang\','07_HumanVoice\general\STRAIGHT\','07_HumanVoice\general\','00_Machine_Learning\'};
-toolboxPath.remotePrefix = repmat({''},1,6);
+toolboxPath.root=''; %root path for your utility,machineLearning, sap toolboxes (by Roger jang), voicebox and pmtk toolbox
+toolboxPath.remote = {'utility','machineLearning','sap','voicebox','pmtk3-1nov12'};
+toolboxPath.remotePrefix = repmat({''},1,6); %if the toolboxes are put in the same root dir but different sub dir, you could specify this Prefix
 for i=1:length(toolboxPath.remote)
 % 	addpath([toolboxPath.root, toolboxPath.remote{i}]);
 	addpath([toolboxPath.root, toolboxPath.remotePrefix{i} toolboxPath.remote{i}]);	
 end
 currDir = pwd; initPmtk3; cd(currDir);
-toolboxPath.local = {'GLOAT','pmtk','sap','STRAIGHT'};
+toolboxPath.local = {'GLOAT','pmtk','sap'}; %path to the files of several toolboxes that has been modified by me.
 for i=1:length(toolboxPath.local)
 	addpath([pwd filesep toolboxPath.local{i}]);
 end
